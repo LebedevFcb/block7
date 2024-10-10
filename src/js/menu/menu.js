6ab11d4306aeeb9dc98ae7c9d284a3287c76b338
LebedevFcb <107355201+LebedevFcb@users.noreply.github.com>
2024-10-10 17:07:53 +0300

    Update menu.js

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -1,44 +1,48 @@
-const btnBurger = document.querySelector('.burger')
-const menuBurger = document.querySelector('.body-content')
-const btnClose = document.querySelector('.burger-close')
+const btnBurger = document.querySelector('.burger');
+const menuBurger = document.querySelector('.body-content');
+const btnClose = document.querySelector('.burger-close');
 
 function clickMenu() {
   btnBurger.addEventListener('click', function () {
-    menuBurger.style.display = 'block'
-    btnBurger.style.display = 'none' // Скрываем кнопку после нажатия
-  })
+    menuBurger.style.display = 'block';
+    btnBurger.style.display = 'none'; // Скрываем кнопку после нажатия
+  });
 }
 
 btnClose.addEventListener('click', function () {
-  menuBurger.style.display = 'none'
-  btnBurger.style.display = 'block' // Показать кнопку снова при закрытии меню
-})
+  menuBurger.style.display = 'none';
+  btnBurger.style.display = 'block'; // Показать кнопку снова при закрытии меню
+});
 
 // Закрытие меню при клике вне его границ
 document.addEventListener('click', function (event) {
-  const isClickInsideMenu = menuBurger.contains(event.target)
-  const isClickOnBurger = btnBurger.contains(event.target)
+  const isClickInsideMenu = menuBurger.contains(event.target);
+  const isClickOnBurger = btnBurger.contains(event.target);
 
-  if (!isClickInsideMenu && !isClickOnBurger) {
-    menuBurger.style.display = 'none'
-    btnBurger.style.display = 'block' // Показываем кнопку снова
+  // Добавляем условие для проверки ширины экрана
+  if (window.innerWidth <= 1120) {
+    if (!isClickInsideMenu && !isClickOnBurger) {
+      menuBurger.style.display = 'none';
+      btnBurger.style.display = 'block'; // Показываем кнопку снова
+    }
   }
-})
+});
 
-clickMenu()
+clickMenu();
 
 function checkScreenSize() {
   if (window.innerWidth > 1420) {
-    menuBurger.style.display = 'block'
-    btnBurger.style.display = 'none' // Скрываем кнопку на больших экранах
+    menuBurger.style.display = 'block';
+    btnBurger.style.display = 'none'; // Скрываем кнопку на больших экранах
   } else {
-    menuBurger.style.display = 'none'
-    btnBurger.style.display = 'block' // Показать кнопку на меньших экранах
+    menuBurger.style.display = 'none';
+    btnBurger.style.display = 'block'; // Показать кнопку на меньших экранах
   }
 }
 
 // Запуск функции при изменении размера окна
-window.addEventListener('resize', checkScreenSize)
+window.addEventListener('resize', checkScreenSize);
 
 // Проверка при загрузке страницы
-checkScreenSize()
+checkScreenSize();
+
